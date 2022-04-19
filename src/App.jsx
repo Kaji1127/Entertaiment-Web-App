1a30a4894178120d921627797747426275b889eb
K.Kishin <riesgraf.birsack.1226@gmail.com>
2022-04-19 12:33:38 +0900

    add react-router library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import './App.scss';
+import Header from './layout/Header/Header';
+import Movie from './pages/Movie/Movie';
+import TV from './pages/TV/TV';
+import Bookmark from './pages/Bookmark/Bookmark';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Header />}>
+					<Route index element={<Home />} />
+					<Route path="movies" element={<Movie />} />
+					<Route path="tv-series" element={<TV />} />
+					<Route path="bookmark" element={<Bookmark />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
